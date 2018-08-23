2c36f69b3338d6d36f05e0574a533cea2e3128f7
Mohos Tamás <tomi@mohos.name>
2018-08-23 11:46:11 +0200

    feat(Arena): Constructor handle one parameter

--- src/BattleArena/Arena.php
+++ src/BattleArena/Arena.php
@@ -4,13 +4,16 @@ namespace BattleArena;
 
 class Arena
 {
+    private $character;
 
-    public function __construct()
+    public function __construct(Character $character)
     {
+        $this->character = $character;
     }
 
     public function getAnnouncement(): string
     {
-        return 'Tamark has won the battle, 32 health left';
+        return sprintf('%s has won the battle, %d health left', $this->character->getName(),
+            $this->character->getHealth());
     }
 }
--- test/BattleArena/ArenaTest.php
+++ test/BattleArena/ArenaTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use BattleArena\Arena;
+use BattleArena\Character;
 use PHPUnit\Framework\TestCase;
 
 class ArenaTest extends TestCase
@@ -10,16 +11,31 @@ class ArenaTest extends TestCase
      */
     private $arena;
 
+    /**
+     * @var Character
+     */
+    private $character;
+
     public function setUp()
     {
-        $this->arena = new Arena();
+        $this->character = new Character('Tamark', 32, 5);
+        $this->arena = new Arena($this->character);
     }
 
     /**
      * @test
      */
-    public function getAnnouncement_ReturnsAnnouncement()
+    public function getAnnouncement_GivenOneCharacter_ReturnsAnnouncement()
     {
         $this->assertEquals('Tamark has won the battle, 32 health left', $this->arena->getAnnouncement());
+    }
+
+    /**
+     * @test
+     */
+    public function getAnnouncement_GivenOneCharacterWith99Health_ReturnsCorrectAnnouncement()
+    {
+        $this->character->setHealth(99);
+        $this->assertEquals('Tamark has won the battle, 99 health left', $this->arena->getAnnouncement());
     }
 }
