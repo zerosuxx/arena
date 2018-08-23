f913e2804544b3c67f29c9d94f464f289a58808b
Mohos Tamás <tomi@mohos.name>
2018-08-23 16:18:28 +0200

    refactor(Enemies): foreach calc sum to array_reduce

--- src/BattleArena/Enemies.php
+++ src/BattleArena/Enemies.php
@@ -21,11 +21,10 @@ class Enemies implements CharacterInterface
 
     public function getHealth(): int
     {
-        $sum = 0;
-        foreach ($this->enemies as $en) {
-            $sum += $en->getHealth();
-        }
-        return $sum;
+        return array_reduce($this->enemies, function ($sum, CharacterInterface $character) {
+            $sum += $character->getHealth();
+            return $sum;
+        });
     }
 
     public function getName(): string
@@ -35,11 +34,10 @@ class Enemies implements CharacterInterface
 
     public function getDamage(): int
     {
-        $sum = 0;
-        foreach ($this->enemies as $en) {
-            $sum += $en->getDamage();
-        }
-        return $sum;
+        return array_reduce($this->enemies, function ($sum, CharacterInterface $character) {
+            $sum += $character->getDamage();
+            return $sum;
+        });
     }
 
     public function setHealth(int $health): void
