206ee6bcf315fd41f54bbf902f269024867c0120
Mohos Tamás <tomi@mohos.name>
2018-08-23 13:15:47 +0200

    refactor(ArenaTest): one correct test

--- test/BattleArena/ArenaTest.php
+++ test/BattleArena/ArenaTest.php
@@ -7,49 +7,15 @@ use PHPUnit\Framework\TestCase;
 class ArenaTest extends TestCase
 {
     /**
-     * @var Arena
-     */
-    private $arena;
-
-    /**
-     * @var Character
-     */
-    private $hero;
-
-    /**
-     * @var Character
-     */
-    private $monster;
-
-    public function setUp()
-    {
-        $this->hero = new Character('Tamark', 50, 4);
-        $this->monster = new Character('Giant Wolf', 16, 6);
-        $this->arena = new Arena($this->hero, $this->monster);
-    }
-
-    /**
      * @test
      */
-    public function getAnnouncement_GivenOneCharacter_ReturnsAnnouncement()
+    public function getAnnouncement_HasTwoCharacter_ReturnsHeroWinsAnnouncement()
     {
-        $this->assertEquals('Tamark has won the battle, 50 health left', $this->arena->getAnnouncement());
-    }
+        $arena = new Arena(
+            new Character('Tamark', 50, 4),
+            new Character('Giant Wolf', 16, 6)
+        );
 
-    /**
-     * @test
-     */
-    public function getAnnouncement_GivenOneCharacterWith99Health_ReturnsCorrectAnnouncement()
-    {
-        $this->hero->setHealth(99);
-        $this->assertEquals('Tamark has won the battle, 99 health left', $this->arena->getAnnouncement());
-    }
-
-    /**
-     * @test
-     */
-    public function getWinner_TwoCharacterFighting_ReturnsHeroWins()
-    {
-        $this->assertEquals($this->hero, $this->arena->getWinner());
+        $this->assertEquals('Tamark has won the battle, 50 health left', $arena->getAnnouncement());
     }
 }
