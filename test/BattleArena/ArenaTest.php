a41bce0d96a2ae26a0170bc3d3b625d1e1e3b499
Mohos Tamás <tomi@mohos.name>
2018-08-23 14:07:59 +0200

    refactor(ArenaTest): add getPlayersAndArena method

--- test/BattleArena/ArenaTest.php
+++ test/BattleArena/ArenaTest.php
@@ -11,10 +11,7 @@ class ArenaTest extends TestCase
      */
     public function getAnnouncement_HasTwoCharacter_ReturnsHeroWinsAnnouncement()
     {
-        $arena = new Arena(
-            new Character('Tamark', 50, 4),
-            new Character('Giant Wolf', 16, 6)
-        );
+        list(, , $arena) = $this->getPlayersAndArena(50, 4, 16, 6);
 
         $this->assertEquals('Tamark has won the battle, 50 health left', $arena->getAnnouncement());
     }
@@ -24,11 +21,7 @@ class ArenaTest extends TestCase
      */
     public function getWinner_HasTwoCharacterAndMonsterBiggerHealth_ReturnsMonsterWins()
     {
-
-        $hero = new Character('Tamark', 50, 4);
-        $monster = new Character('Giant Wolf', 53, 6);
-        $arena = new Arena($hero, $monster);
-
+        list(, $monster, $arena) = $this->getPlayersAndArena(50, 4, 53, 6);
         $this->assertEquals($monster, $arena->getWinner());
     }
 
@@ -37,11 +30,7 @@ class ArenaTest extends TestCase
      */
     public function getWinner_HasTwoCharacterFightingOneAttack_ReturnsHeroWins()
     {
-
-        $hero = new Character('Tamark', 4, 5);
-        $monster = new Character('Giant Wolf', 5, 10);
-        $arena = new Arena($hero, $monster);
-
+        list($hero, , $arena) = $this->getPlayersAndArena(4, 5, 5, 10);
         $arena->fight();
 
         $this->assertEquals($hero, $arena->getWinner());
@@ -53,12 +42,21 @@ class ArenaTest extends TestCase
     public function getWinner_HasTwoCharacterFightingTwoAttack_ReturnsMonsterWins()
     {
 
-        $hero = new Character('Tamark', 5, 5);
-        $monster = new Character('Giant Wolf', 6, 5);
-        $arena = new Arena($hero, $monster);
+        list(, $monster, $arena) = $this->getPlayersAndArena(5, 5, 6, 5);
 
         $arena->fight();
 
         $this->assertEquals($monster, $arena->getWinner());
+    }
+
+    /**
+     * @return array
+     */
+    private function getPlayersAndArena($heroHealth, $heroDamage, $monsterHealth, $monsterDamage): array
+    {
+        $hero = new Character('Tamark', $heroHealth, $heroDamage);
+        $monster = new Character('Giant Wolf', $monsterHealth, $monsterDamage);
+        $arena = new Arena($hero, $monster);
+        return [$hero, $monster, $arena];
     }
 }
