09a4e960822e35008f243613505a098bbdb0dd57
Mohos Tamás <tomi@mohos.name>
2018-08-27 15:24:13 +0200

    feat(AttackAction): add perform method

--- src/BattleArena/AttackAction.php
+++ src/BattleArena/AttackAction.php
@@ -2,11 +2,19 @@
 
 namespace BattleArena;
 
-/**
- * Class AttackAction
- * @package BattleArena
- */
+use BattleArena\Character\CharacterInterface;
+
 class AttackAction
 {
+    private $attacker;
 
+    public function __construct(CharacterInterface $attacker)
+    {
+        $this->attacker = $attacker;
+    }
+
+    public function perform(CharacterInterface $target)
+    {
+        $target->takeDamage($this->attacker->getDamage());
+    }
 }
---
+++ test/BattleArena/AttackActionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use BattleArena\AttackAction;
+use PHPUnit\Framework\TestCase;
+
+class AttackActionTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function perform_TakeDamageHero()
+    {
+        $attackerMock = $this->createMock(\BattleArena\Character\CharacterInterface::class);
+        $attackerMock
+            ->expects($this->once())
+            ->method('getDamage')
+            ->willReturn(1);
+
+        $targetMock = $this->createMock(\BattleArena\Character\CharacterInterface::class);
+        $targetMock
+            ->expects($this->once())
+            ->method('takeDamage');
+
+        $action = new AttackAction($attackerMock);
+        $action->perform($targetMock);
+    }
+}
